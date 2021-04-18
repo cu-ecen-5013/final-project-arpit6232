3334b3782ca2bb2f347da24635502c8e36f0007d
arpit6232 <arpit.savarkar10@gmail.com>
2021-04-18 08:42:14 -0600

    updated code removing sudo

--- jetson_display/gpio.sh
+++ jetson_display/gpio.sh
@@ -2,9 +2,9 @@
 
 # Bash Script to initialize GPIO
 
-sudo groupadd gpio
-sudo usermod -aG gpio root
-su root
+# sudo groupadd gpio
+# sudo usermod -aG gpio root
+# su root
 sudo chgrp gpio /sys/class/gpio/export
 sudo chgrp gpio /sys/class/gpio/unexport
 sudo chmod 775 /sys/class/gpio/export
@@ -19,7 +19,6 @@ echo "out" > /sys/class/gpio/gpio79/direction
 if [[ "$#" -ne 1 ]]  
 then 
 	echo " INVALID NUMBER OF ARGUMENTS SUPPLIED AT RUNTIME"
-	echo " * First Argument should be Directory Path "
 	exit 1
 fi
 
