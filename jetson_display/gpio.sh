1c7a935141c378969f18b9106cec75a0570d78ba
arpit6232 <arpit.savarkar10@gmail.com>
2021-04-18 08:31:43 -0600

    Updated bash script under jetson_display

--- jetson_display/gpio.sh
+++ jetson_display/gpio.sh
@@ -2,10 +2,21 @@
 
 # Basch Script ot initialize GPIO
 
-echo 79 > /sys/class/gpio/export
-echo 0 > /sys/class/gpio/gpio79/value
+sudo groupadd gpio
+sudo usermod -aG gpio root
+su root
+sudo chgrp gpio /sys/class/gpio/export
+sudo chgrp gpio /sys/class/gpio/unexport
+sudo chmod 775 /sys/class/gpio/export
+sudo chmod 775 /sys/class/gpio/unexport
 
-if [ [ -z "$1" ] ]; 
+echo "79" > /sys/class/gpio/export
+chgrp -HR /sys/class/gpio/gpio79
+chmod -R 775 /sys/class/gpio/gpio79
+
+echo "out" > /sys/class/gpio/gpio79/direction
+
+if [[ "$#" -ne 1 ]]  
 then 
 	echo " INVALID NUMBER OF ARGUMENTS SUPPLIED AT RUNTIME"
 	echo " * First Argument should be Directory Path "
@@ -15,13 +26,15 @@ fi
 # if 1st argument is 1, turn on the LED
 if [[ "$1" -eq 1 ]] 
 then
-	echo 1 > /sys/class/gpio/gpio79/value
+	sudo echo 1 > /sys/class/gpio/gpio79/value
 	exit 0
 
 # Check if the 2nd Argument is not Empty
 elif [[ "$1" -eq 0 ]] 
 then	
-	echo 0 > /sys/class/gpio/gpio79/value
+	sudo echo 0 > /sys/class/gpio/gpio79/value
 	exit 0	
 fi
 
+
+echo "79" > /sys/class/gpio/unexport
